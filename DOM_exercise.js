6bda73a49adef3f206f38454de403d1268aa8e51
villhelm-e <villhelm.e@gmail.com>
2021-02-17 08:34:01 -0800

    initial commit

---
+++ DOM_exercise.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('#container');
+
+const container = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
