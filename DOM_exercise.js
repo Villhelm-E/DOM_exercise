27c1317d555e538c74982931418121f104865626
villhelm-e <villhelm.e@gmail.com>
2021-05-27 08:28:25 -0700

    Button using event listener

--- DOM_exercise.js
+++ DOM_exercise.js
@@ -33,6 +33,7 @@ blackpink.style.backgroundColor = 'pink';
 const h1 = document.createElement('h1');
 h1.textContent = "I'm in a div";
 
+//div under div
 const divp = document.createElement('p');
 divp.textContent = "ME TOO!";
 
@@ -41,5 +42,8 @@ blackpink.appendChild(divp);
 
 document.body.appendChild(blackpink);
 
+//Button events
 const btn = document.querySelector('#btn');
-btn.onclick = () => alert("Hello World");
+btn.addEventListener('click', () => {
+    alert("Hello World")
+});
